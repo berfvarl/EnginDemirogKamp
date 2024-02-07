4ee86f3aa9b5add0418336fe461f0eed07e58d3a
berfvarl <106141374+berfvarl@users.noreply.github.com>
2024-02-07 22:06:37 +0300

    Maske Takip Projesi

---
+++ PersonManager.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Concrete;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PersonManager
+    {
+        public void ApplyForMask (Person person)
+        {
+
+        }
+
+        public List<Person> GetList()
+        {
+            return null;
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            return client.TCKimlikNoDogrulaAsync(new TCKimlikNoDogrulaRequest(TCKimlikNoDogrulaRequestBody(12345678910, Ad: "Berfin" , Soyad: "Varli",DogumYili:2002))); 
+        
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace Workaraund
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //SelamVer("ali");n
+            //SelamVer("ayşe");
+            //SelamVer("engin");
+            //SelamVer();
+            //Topla(5, 10);
+
+            ////Arrays 
+            //string[] students = new string[3];
+            //students[0] = "Engin";
+            //students[1] = "kerem";
+            //students[2] = "berkay";
+            //Console.WriteLine("------Foreach------------");
+            //foreach (var student in students)
+            //{
+            //    Console.WriteLine(student);
+            //}
+            //Console.WriteLine("------------For----------------");
+            //for (int i = 0; i < students.Length; i++)
+            //{
+            //    Console.WriteLine(students[i]);
+            //}
+            //string[] sehirler1 =new[] {"Ankara","i","b" };
+            //string[] sehirler2 = new[] { "b", "a", "d" };
+            //sehirler2 = sehirler1;
+            //sehirler1[0] = "adana";
+            //Console.WriteLine(sehirler2[0]);
+            //List<string> sehirler3 = new List<string>();
+            //sehirler3.Add("urfa");
+            //sehirler3.Add("aydın");
+            //foreach (var i in sehirler3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            Person person1 = new Person();
+            person1.FirstName = "Berfin";
+            person1.LastName = "Varli";
+            person1.DateOfBirthYear = 2002;
+            person1.NationalIdentity = 12345678910;
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+        }
+        //public static void SelamVer(string isim="")
+        //{
+        //    string mesaj = "helllo  "+isim;
+        //    Console.WriteLine(mesaj);
+        //}
+
+        //static int Topla(int sayi1,int sayi2)
+        //{
+        //    int sonuc = sayi1 + sayi2;
+        //    Console.WriteLine("Toplam :" + sonuc);
+        //    return sonuc;
+        //}
+    }
+}
+
+
+
+
